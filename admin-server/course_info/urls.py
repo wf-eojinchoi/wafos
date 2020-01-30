c562fd656aeef92b31998558748cc23c7e042a4e
wf-eojinchoi <johannesedelstien.gmail.com>
2020-01-30 18:02:27 +0900

    표준세탁코스페이징관련해서 url추가.

--- admin-server/course_info/urls.py
+++ admin-server/course_info/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path('', views.AgencyDeviceInfoList.as_view(), name='agency-device-info-list'),
     path('agency/<int:pk>', views.AgencyCourseList.as_view(), name='agency-course '),
     #<int:pk> 타입형이 int이고, pk라는 이름으로 값으로 받을거다.
-    #path('standard/<int:pk>', views.StandardCourseList.as_view(), name='standard-course '),
+    path('standard/<int:pk>', views.StandardCourseList.as_view(), name='standard-course '),
     #페이징처리를위한 새 uri
     path('standard/', views.StandardCourseList.as_view(), name='standard-course '),
     path('standard/copy/', views.StandardCourseCopy.as_view(), name='standard-course-copy '),
